89ea342e994464115c1107c14a9ac317390bb7dd
Abhishek Saurabh <abhisheksaurabh2000@gmail.com>
2024-06-22 19:00:23 +0530

    First commit
    First date:06/22/2024

---
+++ ecomapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from ecomapp.models import *
+# Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(Product)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
+admin.site.register(product_category)
---
+++ ecomapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from ecomapp import views
+
+urlpatterns = [
+    path('',views.index ,name='index'),
+    path('contact/',views.contact ,name='contact'),
+    path('about/',views.about ,name='about'),
+    path('checkout/',views.checkout ,name='checkout'),
+    path('handlerequest/',views.handlerequest,name="Handlerequest"),
+    path('profile',views.profile,name='profile'),
+    
+]
